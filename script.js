c146d7d19238c7032987a9a18f4756fc0f886abe
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-07 20:46:46 +0100

    nofull

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+// WHILE
+// let num = 1;
+
+// while (num <= 10) {
+//     console.log(num);
+//     num++;
+//     // ++ це як num + 1
+// }
+
+//DO...WHILE
+// let num = 1;
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 5)
+
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 0)
+
+// FOR
+// for (початок; умова; крок) {
+
+// }
+
+// for (let num = 1; num < 10; num++) {
+//     console.log(num);
+    
+// }
